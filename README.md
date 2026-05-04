adab760b3a455703d056c1e18f521af9047457d3
Tomás S Pita <Tomasspita@MacBookPro.home>
2026-05-04 15:42:31 +0200

    añadido bucle de eventos y archivo .gitignore

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Mi progreso con el libro "Beginning-C-Game-Programming-Third-Edition"
 
 Recordatorio como compilar los juegos
-g++ <nombre_del_juego>.cpp -std=c++17 -lsfml-graphics -lsfml-window -lsfml-system -o <nombre_del_juego>
+g++ <carpetadeljuego/nombre_del_juego>.cpp -std=c++17 -lsfml-graphics -lsfml-window -lsfml-system -o output/<nombre_del_juego>
--- timber/timber.cpp
+++ timber/timber.cpp
@@ -1,5 +1,6 @@
 // Include important libraries here
 #include <SFML/Graphics.hpp>
+#include <optional> 
 
 // Make code easier to type with "using namespace" using namespace sf;
 using namespace sf;
@@ -14,6 +15,45 @@ int main ()
     RenderWindow window(vm, "Timber!!!", State::Fullscreen);
     
 
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<Event::Closed>())
+                window.close();
+        }
+
+        /*
+        ****************************************
+        Handle the players input
+        ****************************************
+        */
+       if (Keyboard::isKeyPressed(Keyboard::Key::Escape))
+       {
+           window.close();
+        }
+        
+        /*
+        ****************************************
+        Update the scene
+        ****************************************
+        */
+       
+       
+       
+       /*
+       ****************************************
+       Draw the scene
+       ****************************************
+       */
+
+       // Clear everything from the last frame window.clear();
+
+       // Draw our game scene here
+
+
+       // Show everything we just drew window.display();
+    }
     return 0; 
 }
 
