dad42178ed2f02b4cb37370256101b10924a69d2
Tomás S Pita <Tomasspita@MacBookPro.home>
2026-05-19 19:23:07 +0200

    agrega ejemplo de loops

---
+++ examples/loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    
+    int z = 0;
+    
+    while(true)
+    {
+        z++; // Bigger... Bigger...
+        std::cout << z;
+
+        break; // No you're not
+        
+        // Code doesnt reach here
+    }
+}
