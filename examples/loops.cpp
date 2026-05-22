a45d1a1adadc4129e050922f17ee109e34ae7400
Tomás S Pita <Tomasspita@MacBookPro.home>
2026-05-22 18:41:32 +0200

    Añade ejemplos de loops while y for

--- examples/loops.cpp
+++ examples/loops.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    
+    using namespace std;
     int z = 0;
     
     while(true)
@@ -13,4 +13,59 @@ int main() {
         
         // Code doesnt reach here
     }
+
+
+    int userInput;
+
+    while (true)
+    {
+        cout << "Enter a positive number to exit: ";
+        cin >> userInput;
+
+        if (cin.fail()){
+
+            cin.clear();
+
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+
+            cout << "Invalid input. Try again." <<endl;
+            continue;
+        }
+
+        if (userInput > 0)
+        {
+            break;
+        }
+        cout << "Invalid input. try again." << endl;
+    }
+
+    int x = 0 ;
+    while (x < 100) {
+
+        x++;
+    
+    }
+
+    int i = 3;
+    int j = 10;
+
+    while (i > 0)
+    {
+
+        cout << i << endl;
+        while (j > 0)
+        {
+            cout << j << endl;
+            j--;
+        }
+        j = 10;
+        i--;
+
+    }
+
+    for (int x = 0; x <= 100; x++)
+    {
+        cout << x << endl;
+    }
+
 }
