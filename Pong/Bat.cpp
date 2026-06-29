942e7fb9c5fcf27558d5075f1cc4e4e964cc4474
Tomás S Pita <Tomasspita@MacBookPro.home>
2026-06-29 17:49:26 +0200

    Crea la clase'Bat'

---
+++ Pong/Bat.cpp
@@ -0,0 +1,45 @@
+#include "Bat.h"
+
+// This is the constructor and it is called
+// When we create an object 
+Bat::Bat(float startX, float startY) 
+    : m_Position(startX, startY)
+{
+    m_Shape.setSize(sf::Vector2f(50,5));
+    m_Shape.setPosition(m_Position);
+}
+
+FloatRect Bat::getPosition()
+{
+        return m_Shape.getGlobalBounds();
+}
+RectangleShape Bat::getShape()
+{
+    return m_Shape;
+}
+void Bat::moveLeft()
+{
+    m_MovingLeft = true;
+}
+void Bat::moveRight()
+{
+    m_MovingRight = true;
+}
+void Bat::stopLeft()
+{
+    m_MovingLeft = false;
+}
+void Bat::stopRight()
+{
+    m_MovingRight = false;
+}
+void Bat::update(time dt)
+{
+    if (m_MovingLeft) {
+        m_Position.x -= m_Speed * dt.asSeconds();
+    }
+    if (m_MovingRight) {
+        m_Position.x += m_Speed * dt.asSeconds();
+    }
+    m_Shape.setPosition(m_Position);
+}
