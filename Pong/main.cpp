d45c141a93184a4ea00af98f90c129c054e64563
Tomás S Pita <Tomasspita@MacBookPro.home>
2026-06-30 18:32:21 +0200

    incia la función main y el game loop inicial

---
+++ Pong/main.cpp
@@ -0,0 +1,88 @@
+#include "Bat.h"
+#include <sstream>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Create a video mode object
+    VideoMode vm({1920, 1080});
+    // Create and open a window for the game
+    RenderWindow window(vm, "pong", State::Fullscreen);
+    int score = 0;
+    int lives = 3;
+
+    // Create a bat a the bottom center of the screen
+    Bat bat(1920 / 2, 1080 -20);
+    // We will add a ball in the next chapter
+    // Create a Text object called HUD
+    Text hud;
+    // A cool retry-style font
+    Font font;
+    font.loadFromFile("fonts/DS-DIGIT.ttf");
+    // Set the font to our retro-style
+    hud.setFont(font);
+    // Make it nice and big
+    hud.setCharacterSize(75);
+    // Choose a color
+    hud.setFillColor(Color::White);
+    hud.setPosition({20,20});
+    // Here is our clock for timing everything
+    Clock clock;
+    while (window.isOpen())
+    {
+        /*
+        Handle the player input
+        *************************
+        *************************
+        *************************
+        */
+
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<Event::Closed>())
+                window.close();
+
+        }
+
+        // Handel the player quitting
+        if (Keyboard::isKeyPressed(Keyboard::Key::Escape));
+        {
+            window.close();
+        }
+        // Handle the pressing and releasing of the arrow keys
+        if (Keyboard::isKeyPressed(Keyboard::Key::A));
+        {
+            bat.moveLeft();
+        }
+        else
+        {
+            bat.stopLeft();
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Key::D));
+        {
+            bat.moveRight;
+        }
+        else
+        {
+            bat.stopRight();
+        }
+        /*
+        Update the bat, the ball and the HUD
+        *************************
+        *************************
+        *************************
+        */
+
+        /*
+        Draw the bat, the ball and the HUD
+        *************************
+        *************************
+        *************************
+        */
+
+
+    }
+    return 0;
+}
