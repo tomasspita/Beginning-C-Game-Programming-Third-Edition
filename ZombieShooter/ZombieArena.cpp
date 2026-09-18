22798dea06e6946e2d7050fd213c310895c386e0
Tomás S Pita <Tomasspita@MacBookPro.home>
2026-09-18 15:35:16 +0200

    agrega detección de colisiones de balas zombies y pickups

--- ZombieShooter/ZombieArena.cpp
+++ ZombieShooter/ZombieArena.cpp
@@ -78,6 +78,10 @@ int main()
     Pickup healthPickup(1);
     Pickup ammoPickup(2);
 
+    // About the game
+    int score = 0;
+    int hiScore = 0; 
+
     // The main game loop
     while (window.isOpen())
     {
@@ -170,7 +174,7 @@ int main()
                                 ammoPickup.setArena(arena);
 
                                 // Create horde of zombies
-                                numZombies = 2500;
+                                numZombies = 6;
                                 // Delete the previously allocated memory (if it exists)
                                 delete[] zombies;
                                 zombies = createHorde(numZombies, arena);
@@ -363,6 +367,72 @@ int main()
             // update the pickups
             healthPickup.update(dtAsSeconds);
             ammoPickup.update(dtAsSeconds);
+
+            // Collision detection
+            // Have any zombies been shot?
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < numZombies; j++)
+                {
+                    if (bullets[i].isInFlight() &&
+                        zombies[j].isAlive())
+                    {
+                        if (bullets[i].getPosition().findIntersection
+                            (zombies[j].getPosition()))
+                        {
+                            // Stop the bullet
+                            bullets[i].stop();
+                            // Register the hit and see if it was a kill
+                            if (zombies[j].hit())
+                            {
+                                // Not just a hit but a kill too
+                                score += 10;
+                                if (score >= hiScore)
+                                {
+                                    hiScore = score;
+                                }
+                                numZombiesAlive--;
+                                // When all the zombies are dead (again)
+                                if (numZombiesAlive == 0) {
+                                    state = State::LEVELING_UP;
+                                }
+                            }
+                        }
+                    }
+                }
+            } // End zombie being shot
+
+            // Have any zombies touched the player
+            for (int i = 0; i < numZombies; i++)
+            {
+                if (player.getPosition().findIntersection(
+                    zombies[i].getPosition()) && zombies[i].isAlive())
+                {
+                    if (player.hit(gameTimeTotal))
+                    {
+                        // More here later
+                    }
+                    if (player.getHealth() <= 0)
+                    {
+                        state = State::GAME_OVER;
+                    }
+                }
+            }// End player touched
+
+            // Has the player touched health pickup
+            if (player.getPosition().findIntersection(healthPickup.getPosition())
+                 && healthPickup.isSpawned())
+            {
+                player.increaseHealthLevel(healthPickup.gotIt());
+            }
+
+            // Has the player touched ammo pickup
+            if (player.getPosition().findIntersection(ammoPickup.getPosition())
+                 && ammoPickup.isSpawned())
+            {
+                bulletsSpare += ammoPickup.gotIt();
+            }
+
         } // End updating the scene
 
         /*
